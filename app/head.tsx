5fde36d3a439c3fdd6a570ea7a1e42e58f0abb6b
osvaldo <iosvaldo.app@gmail.com>
2023-02-23 01:38:29 -0500

    initialized app ⏳

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+export default function Head() {
+  return (
+    <>
+      <title>ChatGPT beta</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.css'
+
+
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center text-white h-screen px-2">
+     <h1 className="text-5xl font-bold mb-20"> ChatGPT Messanger</h1>
+    </div>
+  )
+}
+
+export default HomePage
