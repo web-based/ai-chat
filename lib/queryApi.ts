31cb9fa7c0a8acb5ce1d1ab3ed86e695a813d32f
osvaldo <iosvaldo.app@gmail.com>
2023-03-30 00:20:34 -0400

    revert sidebar commit ⏱️

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -11,7 +11,7 @@ const query = async (prompt: string, chatId: string, model: string) => {
     presence_penalty: 0,
   }).then(res => res.data.choices[0].text)
     .catch(err =>
-      `ChatGPT was unable to find an answer for that! (Error: ${err.message})`
+      `ChatGP was unable to find an answer for that!(Error: ${err.message})`
   );
   return res;
 };
